c25d8f017fee994e9feee792b8460f0a17e4794b
Pepereiram <jpepereira.m@gmail.com>
2022-12-06 23:21:16 -0300

    Carpunkcho version devlog 8

--- project/Scenes/Entities/Carpincho.gd
+++ project/Scenes/Entities/Carpincho.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 var hp = 3
-export var speed = 10
+export var speed = 150
 var acceleration = 50
 var velocity = Vector2()
 var direction = 1
@@ -38,10 +38,8 @@ func _physics_process(_delta):
 	if velocity.x == 0:
 		playback.travel("idle")
 	elif velocity.x > 0:
-		print("derecha")
 		playback.travel("move_right")
 	else:
-		print("izquierda")
 		playback.travel("move_left")
 
 
--- project/menu/PauseMenu/PauseMenu.gd
+++ project/menu/PauseMenu/PauseMenu.gd
@@ -7,6 +7,7 @@ onready var exit = $"%exit"
 onready var master_volume = $"%MasterS"
 onready var music_volume = $"%MusicS"
 onready var sfx_volume = $"%sfxS"
+onready var bgm = $AudioStreamPlayer
 
 func _ready():
 	resume.connect("pressed",self, "_on_resume_pressed")
@@ -16,23 +17,31 @@ func _ready():
 	master_volume.connect("value_changed", self, "_on_masterV_value_changed")
 	music_volume.connect("value_changed", self, "_on_musicV_value_changed")
 	sfx_volume.connect("value_changed", self, "_on_sfxV_value_changed")
+	bgm.stop()
 	hide()
 
 func _input(event):
 	if event.is_action_pressed("pauseMenu"):
+		if bgm.playing:
+			bgm.stop()
+		else:
+			bgm.play()
 		visible = !visible
 		get_tree().paused = visible
 
 func _on_resume_pressed():
+	bgm.stop()
 	hide()
 	get_tree().paused = false
 
 
 func _on_restart_pressed():
+	bgm.stop()
 	get_tree().reload_current_scene()
 	get_tree().paused = false
 	
 func _on_main_menu_pressed():
+	bgm.stop()
 	get_tree().paused = false
 	get_tree().change_scene("res://menu/Mainmenu.tscn")
 	
