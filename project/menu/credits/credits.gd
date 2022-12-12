fa11a36705f83f3603c45d1a267232ee06827a10
oppallios <atommya.2@gmail.com>
2022-12-12 13:57:29 -0300

    credits :0

---
+++ project/menu/credits/credits.gd
@@ -0,0 +1,32 @@
+extends MarginContainer
+
+
+var scroll_speed = 1
+
+onready var scroll_container = $ScrollContainer
+onready var credits_container = $ScrollContainer/CreditsContainer
+
+var scroll_ended = false
+
+func _ready():
+	scroll_container.scroll_vertical = 0
+	set_physics_process(false)
+	yield(get_tree().create_timer(1), "timeout")
+	set_physics_process(true)
+	
+
+
+func _physics_process(delta):
+	var last_scroll = scroll_container.scroll_vertical
+	scroll_container.scroll_vertical += scroll_speed
+	var new_scroll = scroll_container.scroll_vertical
+	if last_scroll == new_scroll:
+		_to_main_menu()
+		
+
+
+func _to_main_menu():
+	if not scroll_ended:
+		scroll_ended = true
+		yield(get_tree().create_timer(2), "timeout")
+		get_tree().change_scene("res://menu/Mainmenu.tscn")
