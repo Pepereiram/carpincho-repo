dbb459f7bfe17f33c984b9bbcbac2d742d355319
oppallios <atommya.2@gmail.com>
2022-12-15 19:26:34 -0300

    animation fixed lol

--- project/menu/credits/dancing_animation.gd
+++ project/menu/credits/dancing_animation.gd
@@ -5,9 +5,10 @@ onready var anim_player =$AnimationPlayer
 onready var playback = anim_tree.get("parameters/playback")
 onready var anim1 
 onready var timer = $Timer
-var velocity = 3
+var velocity = 200
 var distance = 0
 var celebrate = true
+var awa = 0
 
 func _ready():
 	anim_tree.active = true
@@ -17,13 +18,13 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if anim1:
-		position.x += 2.5
-		if position.x > 600:
+		position.x += 200 * delta
+		if position.x > 600 :
 			set_process(false)
 			Fade.change_scene("res://menu/credits/ending_animation2.tscn")
 	else:
-		position.x += velocity
-		distance += 3
+		position.x += velocity  *  delta
+		distance += 200 * delta
 		if distance > 300:
 			velocity = 0
 			if celebrate:
